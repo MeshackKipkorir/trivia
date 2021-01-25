3733ed6a38f9577c8469dbfc7052f826aa4d91c0
MeshackKipkorir <meshackkipkorir105@gmail.com>
2021-01-25 13:29:45 +0300

    initial commit

---
+++ trivia/app/Http/Controllers/PlayerController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Http\Resources\Player as PlayerResource;
+use App\Http\Resources\PlayerCollection;
+
+class PlayerController extends Controller
+{
+    //
+    public function index(){
+
+    }
+}
