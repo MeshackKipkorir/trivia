466e2b85c390be8fc2f85ceb7386e408cc93a72c
MeshackKipkorir <meshackkipkorir105@gmail.com>
2021-01-26 14:52:34 +0300

    added skeleton components

--- trivia/trivia-web-client-vue/src/main.js
+++ trivia/trivia-web-client-vue/src/main.js
@@ -1,8 +1,29 @@
 import Vue from 'vue'
-import App from './App.vue'
+import VueRouter from 'vue-router'
 
 Vue.config.productionTip = false
 
+Vue.use(VueRouter);
+
+import Dashboard from './components/Dashboard.vue';
+import TriviaGame from './components/TriviaGame';
+const routes = [
+    {
+        path:'/',
+        component:Dashboard,
+    },
+    {
+        path:'/trivia',
+        component: TriviaGame
+    },
+]
+
+const router = new VueRouter({
+    mode:'history',
+    routes
+})
+
 new Vue({
-  render: h => h(App),
+    router,
+    render: h=>h(Dashboard)
 }).$mount('#app')
